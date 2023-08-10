712e21ae04604bcf1e885ef79cfd0f1538115344
heejinnn <gml0593@gmail.com>
2023-08-10 11:51:16 +0900

    #8 feat create project

---
+++ fit-a-pet-client/fit-a-pet-client/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fit-a-pet-client
+//
+//  Created by 최희진 on 2023/08/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
